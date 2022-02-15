72e3ae6d939dbf3e6252d1c80e1aa3dc352dc92c
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-02-15 20:22:46 +0100

    style: formating

--- file.h
+++ file.h
@@ -1,6 +1,6 @@
 
-void readFirstLine(char *file,char *line);
+void readFirstLine(char *file, char *line);
 void readOneLine(char *file, char *s, int N);
 int fgetint(char *file);
-void fgetString(char *file,char *s);
-void stdoutOneline(char *s_in,char *s_out);
+void fgetString(char *file, char *s);
+void stdoutOneline(char *s_in, char *s_out);
--- libafile.c
+++ libafile.c
@@ -1,21 +1,19 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "file.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXLINE 80
 
-void readFirstLine(char *file,char *line){
+void readFirstLine(char *file, char *line) {
   FILE *fp = NULL;
   fp = fopen(file, "r");
   if (!fp) {
-    perror ("File open error!\n");
+    perror("File open error!\n");
   }
   fscanf(fp, "%[^\n]", line);
   fclose(fp);
 }
-
-
 
 void readOneLine(char *file, char *s, int N) {
   FILE *fp = fopen(file, "r");
@@ -39,19 +37,19 @@ int fgetint(char *file) {
   int i = 0;
   int x = 0;
 
-  fgets(line, sizeof(line),fp);
+  fgets(line, sizeof(line), fp);
   sscanf(line, "%d", &x);
   fclose(fp);
   return x;
 }
 
-void fgetString(char *file,char *s) {
+void fgetString(char *file, char *s) {
   FILE *fp = fopen(file, "r");
-  fgets(s, sizeof(s),fp);
+  fgets(s, sizeof(s), fp);
 }
 
-// run shell command and get first output from stdout 
-void stdoutOneline(char *s_in,char *s_out) {
+// run shell command and get first output from stdout
+void stdoutOneline(char *s_in, char *s_out) {
   FILE *popen(const char *command, const char *mode);
   int pclose(FILE * stream);
   FILE *file;
